e62cb0dfab3d79f1fc6b8975d8ffaa68ee1561da
SHALINI <shalinisureshshaluu@gmail.com>
2026-06-09 22:19:16 +0530

    Add files via upload

---
+++ if else .py
@@ -0,0 +1,15 @@
+i="y"
+while i=="y":
+    a=input("enter a colour")
+    if a=='green':
+        print( "u can go")
+    elif a=='yellow':
+        print("get ready")
+    elif a=='red':
+        print("stop")
+    else:
+        print("invalid")
+    i=input("do you want to continue: y/n")
+      
+    
+   
---
+++ plaindrome.py
@@ -0,0 +1,5 @@
+str1=input()
+if str1 == str1 [ ::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
